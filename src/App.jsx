043fd6629c544582197f51ff5502238ad84433fd
Tarikuzzaman12 <tarikuzzamantuhin920@gmail.com>
2024-10-22 09:18:48 +0600

    add some components,set layout and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 
@@ -10,7 +11,10 @@ function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+     <div className='md:flex'>
+     <Blogs></Blogs>
+     <Bookmarks></Bookmarks>
+     </div>
     </>
   )
 }
